383affa44810f89b621ef82e965fa20847db41de
Cristian Pereira <pereiracamilo51@gmail.com>
2025-11-23 11:43:05 -0500

    feat(Auth): Se creo el funcionamiento de recuperar contraseña

--- routes.js
+++ routes.js
@@ -8,5 +8,6 @@ const router = express.Router();
 router.get('/auth/login', Auth.login);
 router.post('/auth/register', Auth.register);
 router.post('/auth/logout', authenticate, Auth.logout);
+router.post('/auth/forgot-password', Auth.forgotPassword);
 
 module.exports = router;
