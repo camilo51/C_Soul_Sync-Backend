7e8cade1b010d8c244fc275ffd2aa9404bce288e
Sebastian Velez <sebas.0927.svm@gmail.com>
2025-11-26 10:01:47 -0500

    chore: verificacion del email

--- utils/email.js
+++ utils/email.js
@@ -32,13 +32,30 @@ const templates = [
     },
     {
         name: 'verify-account',
-        template: 'template_welcome123',
         html: (name, token) => `
-            <div>
-                <h1>Welcome to Our App!</h1>
-                <p>Thank you for registering. Please verify your account by clicking the link below:</p>
-                <a href="https://yourapp.com/verify-account?email={{email}}">Verify Account</a>
-            </div>
+            <style>
+                *, *::after, * ::before{
+                    margin: 0;
+                    padding: 0;
+                    font-family:Arial, Helvetica, sans-serif;
+                }
+            </style>
+            <body>
+                <div>
+                    <h1 style="background-color: #101010; padding: 10px; color: #FFFFFF; text-align: center;">C-Soul Sync</h1>
+                    <div style="padding: 20px; text-align: start;">
+                        <p style="margin-bottom: 5px; font-weight: bold;">Hola, ${name}</p>
+                        <p>Solicitud de verificación de correo electrónico para tu cuenta. Has clic en el siguiente botón para continuar con la verificación</p>
+                        <div style="display: flex; justify-content: center; align-items: center;">
+                            <a href="${process.env.FRONTEND_URL}/verified-email?token=${token}" style="padding: 15px; display: inline-block; background-color: #101010; color: #FFFFFF; text-decoration: none; border-radius: 5px; font-weight: bold; margin-block: 20px">Verificar Email</a>
+                        </div>
+                        <p>Si usted no solicitó esta acción, no se requiere hacer nada.</p>
+
+                        <p style="margin-top: 10px; font-size: 13px;">En caso de que el boton anterior no funcione, utilice este enlace</p>
+                        <a href="${process.env.FRONTEND_URL}/verified-email?token=${token}" style="font-size: 13px;">${process.env.FRONTEND_URL}/verified-email?token=${token}</a>
+                    </div>
+                </div>
+            </body>
         `
     }
 ]
