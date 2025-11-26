97f0c6c450ec3d683f95f2a46d53f7e70052adc1
Sebastian Velez <sebas.0927.svm@gmail.com>
2025-11-26 15:05:40 -0500

    fix: corregir errores ortográficos en plantillas de email

--- utils/email.js
+++ utils/email.js
@@ -17,13 +17,13 @@ const templates = [
                     <h1 style="background-color: #101010; padding: 10px; color: #FFFFFF; text-align: center;">C-Soul Sync</h1>
                     <div style="padding: 20px; text-align: start;">
                         <p style="margin-bottom: 5px; font-weight: bold;">Hola, ${name}</p>
-                        <p>Solicitud de recuperación de contraseña para tu cuenta. Has clic en el siguiente botón para continuar con la recuperación</p>
+                        <p>Solicitud de recuperación de contraseña para tu cuenta. Haz clic en el siguiente botón para continuar con la recuperación</p>
                         <div style="display: flex; justify-content: center; align-items: center;">
                             <a href="${process.env.FRONTEND_URL}/reset-password?token=${token}" style="padding: 15px; display: inline-block; background-color: #101010; color: #FFFFFF; text-decoration: none; border-radius: 5px; font-weight: bold; margin-block: 20px">Recuperar Contraseña</a>
                         </div>
                         <p>Si usted no solicitó esta acción, no se requiere hacer nada.</p>
 
-                        <p style="margin-top: 10px; font-size: 13px;">En caso de que el boton anterior no funcione, utilice este enlace</p>
+                        <p style="margin-top: 10px; font-size: 13px;">En caso de que el botón anterior no funcione, utilice este enlace</p>
                         <a href="${process.env.FRONTEND_URL}/reset-password?token=${token}" style="font-size: 13px;">${process.env.FRONTEND_URL}/reset-password?token=${token}</a>
                     </div>
                 </div>
@@ -43,15 +43,15 @@ const templates = [
             <body>
                 <div>
                     <h1 style="background-color: #101010; padding: 10px; color: #FFFFFF; text-align: center;">C-Soul Sync</h1>
-                    <div style="padding: 20px; text-align: start;">
+                    <div style="padding: 20px; text-align: start%;">
                         <p style="margin-bottom: 5px; font-weight: bold;">Hola, ${name}</p>
-                        <p>Solicitud de verificación de correo electrónico para tu cuenta. Has clic en el siguiente botón para continuar con la verificación</p>
+                        <p>Solicitud de verificación de correo electrónico para tu cuenta. Haz clic en el siguiente botón para continuar con la verificación</p>
                         <div style="display: flex; justify-content: center; align-items: center;">
                             <a href="${process.env.FRONTEND_URL}/verified-email?token=${token}" style="padding: 15px; display: inline-block; background-color: #101010; color: #FFFFFF; text-decoration: none; border-radius: 5px; font-weight: bold; margin-block: 20px">Verificar Email</a>
                         </div>
                         <p>Si usted no solicitó esta acción, no se requiere hacer nada.</p>
 
-                        <p style="margin-top: 10px; font-size: 13px;">En caso de que el boton anterior no funcione, utilice este enlace</p>
+                        <p style="margin-top: 10px; font-size: 13px;">En caso de que el botón anterior no funcione, utilice este enlace</p>
                         <a href="${process.env.FRONTEND_URL}/verified-email?token=${token}" style="font-size: 13px;">${process.env.FRONTEND_URL}/verified-email?token=${token}</a>
                     </div>
                 </div>
